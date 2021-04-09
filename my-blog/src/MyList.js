cc831879f6c16627fc96af24a652834809f21090
Naveen Jakhar <naveen.jakhar11602376@gmail.com>
2021-04-10 00:49:42 +0530

    add file

---
+++ my-blog/src/MyList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import db from './Firebase_config';
+export const MyList = ({ todo, id }) => {
+    const deleteitem = () => {
+        db.collection("todos").doc(id).delete();
+        console.log("button toggle");
+    }
+    return (
+        <>
+            <div id="Datafieldb" className="col-11 col-md-12  mt-3 ml-md-3 ml-3 shadow-lg">
+                <div className="row">
+                <div className="col-12 ot pt-3">
+                <div className="justify-content-center text-center"><h5> New Post</h5></div>
+                    <div className="row">
+                        <div  id="Datafieldw" className="col-12">
+                        <div class="overlay1 ml-n3"></div>
+                        <div class="button mt-2" onClick={deleteitem}><a class="mr-2" href="#"> <i class="fas fa-trash-alt fa-1x my-auto"></i> </a></div>
+
+                        <div id="dda">{todo }</div>
+                        
+                        </div>
+                       
+                    </div>
+                    
+                </div>
+                    {/* <div id="Datafieldw" className="col-11 col-md-10 mb-4 justify-content-center text-center"><p>{todo}</p>
+
+                    </div>
+                    <div className="col-1 col-md-2 mb-4" id="Datafield1">
+                        <div id="delete" onClick={deleteitem} className=" justify-content-center mt-5 shadow-lg ">
+                            <i class="fas fa-trash-alt fa-1x my-auto"></i></div></div> */}
+
+                </div>
+
+            </div>
+        </>
+    )
+}
---
+++ my-blog/src/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+var firebaseConfig = {
+  apiKey: "AIzaSyC8bKPMJ-jhhY6WUsWCga8CR5XKQ2ad1yA",
+  authDomain: "my-blog-88a97.firebaseapp.com",
+  projectId: "my-blog-88a97",
+  storageBucket: "my-blog-88a97.appspot.com",
+  messagingSenderId: "111527265125",
+  appId: "1:111527265125:web:00080b8e0700fd474f978c",
+  measurementId: "G-YMRSKKPNL1"
+};
+  
+  const fire=firebase.initializeApp(firebaseConfig);
+
+  firebase.analytics();
+  export default fire;
